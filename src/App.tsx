03492b006142c7e0dd072462949ab4061d3cf34d
Ritik-Gupta-675 <ritikguptawork675@gmail.com>
2025-06-17 12:28:33 +0530

    Added Blogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Home/home';
+import Home from './pages/home';
 
 // Placeholder components for routes
 const About = () => <div className="min-h-screen pt-20 px-4">About Us</div>;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -37,7 +37,7 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="flex items-center">
-              <img src={logo} alt="Logo" className="w-30 h-16" />
+              < img src={logo} alt="Logo" className="w-30 h-16" />
             </Link>
           </div>
 
